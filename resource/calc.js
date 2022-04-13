05cc7099f524bfad476d718d7c7e4f8169d488a7
Ashay Jaiswal <ashaywin@gmail.com>
2022-04-14 01:05:44 +0530

    Add enter num on screen

--- resource/calc.js
+++ resource/calc.js
@@ -1,11 +1,25 @@
 const calcScreen = document.querySelector("#calc-screen");
 const clearBut = document.querySelector(".clear-but");
 const numButs = document.getElementsByClassName("num-but");
+const numButsArr = [...numButs];
 
-function addNumToScreen(value){
-    calcScreen.innerText += value;
+function addNumToScreen(numBut){
+    if (calcScreen.innerText === "0") {
+        calcScreen.innerText = "";
+    }
+
+    if (calcScreen.innerText.length < 11) {
+        numValue = numBut.target.value;
+        calcScreen.innerText += numValue;
+    }
 }
 
-function clearBut() {
-    calcScreen.innerText = "";
+function clearScreen() {
+    calcScreen.innerText = "0";
 }
+
+clearBut.addEventListener('click', clearScreen);
+
+numButsArr.forEach(element => {
+    element.addEventListener('click', addNumToScreen)
+});
